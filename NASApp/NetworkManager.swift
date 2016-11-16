8dec6fe2cb944c71bbdfb2939747cbef99b8b3e7
Dennis Parussini <thedan84@bitbucket.org>
2016-11-16 17:34:43 +0100

    Added Nuke

---
+++ NASApp/NetworkManager.swift
@@ -0,0 +1,21 @@
+//
+//  NetworkManager.swift
+//  NASApp
+//
+//  Created by Dennis Parussini on 16-11-16.
+//  Copyright © 2016 Dennis Parussini. All rights reserved.
+//
+
+import Foundation
+
+/*
+ API Key NASA API: D3z09rlgrrJlvHcp9V4jAYeNAVJib75bBxqyZiFR
+*/
+
+enum Endpoint {
+    
+}
+
+struct NetworkManager {
+    
+}
