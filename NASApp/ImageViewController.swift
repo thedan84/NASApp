047d48e5f5d3fa5d043fcf4485a6a26cc0986729
Dennis Parussini <thedan84@bitbucket.org>
2016-11-29 16:36:05 +0100

    Added SearchEarthImageVC and ImageVC. Added ability to search for contact and fetch earth image for contact.

---
+++ NASApp/ImageViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ImageViewController.swift
+//  NASApp
+//
+//  Created by Dennis Parussini on 29-11-16.
+//  Copyright © 2016 Dennis Parussini. All rights reserved.
+//
+
+import UIKit
+import Nuke
+
+class ImageViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var photo: EarthPhoto?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        guard let photo = photo, let url = photo.imageURL else { return }
+        
+        Nuke.loadImage(with: url, into: imageView)
+        
+    }
+}
